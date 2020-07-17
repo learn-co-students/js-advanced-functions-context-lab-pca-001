b3a7131b19add20c2adbaa205f360a269e5bbe00
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-17 14:15:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeArray) {
+  let employee = {}
+
+  employee.firstName = employeeArray[0]
+  employee.familyName = employeeArray[1]
+  employee.title = employeeArray[2]
+  employee.payPerHour = employeeArray[3]
+  employee.timeInEvents = []
+  employee.timeOutEvents = []
+
+  return employee
+}
+
+function createEmployeeRecords(employees) {
+  return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(timeIn) {
+  let timeInEvent = {}
+  const time = timeIn.split(" ")
+
+  timeInEvent.type = "TimeIn"
+  timeInEvent.date = time[0]
+  timeInEvent.hour = parseInt(time[1])
+  this.timeInEvents.push(timeInEvent)
+
+  return this
+}
+
+function createTimeOutEvent(timeOut) {
+  let timeOutEvent = {}
+  const time = timeOut.split(" ")
+
+  timeOutEvent.type = "TimeOut"
+  timeOutEvent.date = time[0]
+  timeOutEvent.hour = parseInt(time[1])
+  this.timeOutEvents.push(timeOutEvent)
+
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => {
+    return event.date === date
+  }).hour
+
+  const timeOut = this.timeOutEvents.find(event => {
+    return event.date === date
+  }).hour
+
+  return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(date) {
+  const hours = hoursWorkedOnDate.call(this, date)
+  const pay = this.payPerHour
+
+  return hours * pay
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +79,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(employees) {
+    const wagesArr = employees.map(employee => {
+      return allWagesFor.call(employee)
+    })
+
+    return wagesArr.reduce((wage, total) => wage + total)
+}
+
+function findEmployeeByFirstName(employeeRecords, firstName) {
+    return employeeRecords.find(employee => employee.firstName == firstName)
 }
