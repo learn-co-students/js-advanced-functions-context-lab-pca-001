270add7ec2f95a66ff1a53e08796164dfd51ffe3
Liam Tinney <ltinney@powerhrg.com>
2020-07-20 09:27:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-/* Your Code Here */
+// let allWagesFor = function () {
+//     let eligibleDates = this.timeInEvents.map(function (e) {
+//         return e.date
+//     })
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+//     let payable = eligibleDates.reduce(function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+//     return payable
+// }
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+function createEmployeeRecord(array) {
+  const [firstName, familyName, title, payPerHour] = array
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    "timeInEvents": [],
+    "timeOutEvents": []
+  }
+}
 
-    return payable
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateStamp) {
+  const time = dateStamp.split(" ")
+  // A date stamp ("YYYY-MM-DD HHMM")
+  const updateEmployeeRec = {
+    type: "TimeIn",
+    hour: parseInt(time[1]),
+    date: time[0]
+  }
+  this.timeInEvents.push(updateEmployeeRec)
+  return this
+}
+
+function createTimeOutEvent(dateStamp) {
+  const time = dateStamp.split(" ")
+  // A date stamp ("YYYY-MM-DD HHMM")
+  const updateEmployeeRec = {
+    type: "TimeOut",
+    hour: parseInt(time[1]),
+    date: time[0]
+  }
+  this.timeOutEvents.push(updateEmployeeRec)
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((event) => event.date === date)
+  const timeOut = this.timeOutEvents.find((event) => event.date === date)
+
+  if( timeIn && timeOut ) {
+    return (timeOut.hour - timeIn.hour) / 100
+  }
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function allWagesFor() {
+  return this.timeInEvents.reduce((total, dateStamp) => total + wagesEarnedOnDate.call(this, dateStamp.date), 0)
+}
+
+function calculatePayroll(employees) {
+    // return Sum of pay owed to all employees for all dates, as a number
+  return employees.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+}
+
+function findEmployeeByFirstName(freshArray, firstName) {
+  return freshArray.find(employee => employee.firstName === firstName)
 }
